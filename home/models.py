14878e578879e9a9cc7ba38596b9e938be08712f
Liteb0t <mpandccfanclub@gmail.com>
2024-03-18 11:49:00 +0000

    Pull request test two

--- home/models.py
+++ home/models.py
@@ -2,7 +2,6 @@ import uuid
 from django.db import models
 
 
-# Create your models here.
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
