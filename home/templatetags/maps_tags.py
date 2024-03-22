d29b384d12f61417d838987a599b754d47c25864
ok_nabeelah <110231886+oknabeelah@users.noreply.github.com>
2024-03-22 16:30:04 +0000

    just comments

--- home/templatetags/maps_tags.py
+++ home/templatetags/maps_tags.py
@@ -3,7 +3,7 @@ register = template.Library()
 
 
 @register.filter
-def list_item(lst, i):
+def list_item(lst, i): #define list item, allows you to loop through every doc to get doc table
     try:
         return lst[i]
     except:
